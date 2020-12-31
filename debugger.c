b6cc37f60935495a89b6a6c1f8cc7afe2a509cdc
ergunirem <iremthedeveloper@gmail.com>
2020-12-31 10:11:40 +0100

    adjust_width with filler == '0'

--- debugger.c
+++ debugger.c
@@ -290,6 +290,8 @@ char	*adjust_precision(t_data *data, char *to_print)
 
 void	apply_flag(t_data *data, int *width, char *alignment, char *filler)
 {
+	if (!data->flag)
+		return ;
 	*width = (int)ft_atoi(data->width);
 	if (ft_strchr(data->flag, '-') || *width < 0)
 		*alignment = 'L';
@@ -301,18 +303,25 @@ char	*adjust_width(char *to_print, int width, char alignment, char filler)
 {
 	char	*tmp;
 	char	*pad;
+	char	*padded_str;
+	int		is_negative;
 
+	is_negative = 0;
+	if (to_print[0] == '-')
+		is_negative = 1;
 	if (width > (int)ft_strlen(to_print))
 	{
+		sign = ft_substr(to_print, 0 , is_negative);
 		pad = (char *)ft_calloc(width - ft_strlen(to_print) + 1, sizeof(char));
 		pad = ft_memset(pad, filler, width  - ft_strlen(to_print)); //another func?
 		if (alignment == 'L')
 			tmp = ft_strjoin(to_print, pad); //3
-		// if (alignment == 'R' && target == '0') // && data->type > integers!
-		// {
-		// 	s_fill = ft_strjoin(pad, to_print + check_sign); //1
-		// 	tmp = ft_strjoin(s_sign, s_fill); //2
-		// }
+		if (alignment == 'R' && filler == '0') // && data->type > integers! & negative numbers?
+		{
+			// s_fill = ft_strjoin(pad, to_print + check_sign); //1
+			// tmp = ft_strjoin(s_sign, s_fill); //2
+			tmp = ft_strjoin(pad, to_print);
+		}
 		if (alignment == 'R' && filler != '0') //what happens when check_sign = 0;
 			tmp = ft_strjoin(pad, to_print); //1
 		free(pad);
@@ -403,89 +412,224 @@ int	ft_printf(const char *format, ...)
 	return(format_len);
 }
 
-int main(void)
-{
-	int	ptr;
-	unsigned char c = 0x7f;
-
-	ptr = 8;
-
-	//my func
-	printf("\n%s--ft func--%s\n\n", RED, RESET);
-	ft_printf("\nreturn value = %d", ft_printf("Regular argument\n\n%c \n%.4s \n%p \n%.8d \n|%-9.06u| \n%.0d \n%X", 'X', "mahmut", &ptr, -1223, 999, 0, c));
-	ft_printf("\n");
-
-	// ft_printf("%.6u", 999);
-
-	//lib func
-	printf("\n%s--library func--%s\n\n", RED, RESET);
-	printf(  "\nreturn value = %d\n", printf("Regular argument\n\n%-c \n%.4s \n%p \n%.8d \n|%9.06u| \n%.0d \n%X", 'X', "mahmut", &ptr, -1223, 999, 0, c));
-	return (0);
-}
-
 // int main(void)
 // {
-// 	int				di = 28;
-// 	printf("\n%s/////////////////////////////////////////////////////////////////////////////////////////////////////%s\n\n", YELLOW, RESET);
-// 	printf("%sHandling precision%s (controls the max number of characters to print)\n", PURPLE, RESET);
-// 	printf("%sA dot (.) followed by a number > %%.5d or %%.02d\n\n%s", GREEN, RESET);
+// 	int	ptr;
+// 	unsigned char c = 0x7f;
 
-// 	printf(" (%d)\n", printf("(0) P. 0: |%.0i|", 0));
-// 	ft_printf(" (%d)\n", ft_printf("(0) P. 0: |%.0i|", 0));
-// 	printf(" (%d)\n", printf("(0) P. 1: |%.1i|", 0));
-// 	ft_printf(" (%d)\n", ft_printf("(0) P. 1: |%.1i|", 0));
-// 	printf(" (%d)\n", printf("(0) P. 2: |%.2i|", 0));
-// 	ft_printf(" (%d)\n", ft_printf("(0) P. 2: |%.2i|", 0));
-// 	printf(" (%d)\n", printf("P. 0: |%.0i|", di));
-// 	ft_printf(" (%d)\n", ft_printf("P. 0: |%.0i|", di));
-// 	printf(" (%d)\n", printf("P. 1: |%.1i|", di));
-// 	ft_printf(" (%d)\n", ft_printf("P. 1: |%.1i|", di));
-// 	printf(" (%d)\n", printf("P. 2: |%.2i|", di));
-// 	ft_printf(" (%d)\n", ft_printf("P. 2: |%.2i|", di));
-// 	printf(" (%d)\n", printf("P. 3: |%.3i|", di));
-// 	ft_printf(" (%d)\n", ft_printf("P. 3: |%.3i|", di));
-// 	printf(" (%d)\n", printf("P. 4: |%.4i|", di));
-// 	ft_printf(" (%d)\n", ft_printf("P. 4: |%.4i|", di));
-// 	printf(" (%d)\n", printf("P. 5: |%.5i|", di));
-// 	ft_printf(" (%d)\n", ft_printf("P. 5: |%.5i|", di));
-// 	printf(" (%d)\n", printf("P. 10: |%.10i|", di));
-// 	ft_printf(" (%d)\n", ft_printf("P. 10: |%.10i|", di));
-// 	printf(" (%d)\n", printf("P. 15: |%.15i|", di));
-// 	ft_printf(" (%d)\n", ft_printf("P. 15: |%.15i|", di));
-// 	printf(" (%d)\n", printf("P. 42: |%.42i|", di));
-// 	ft_printf(" (%d)\n", ft_printf("P. 42: |%.42i|", di));
+// 	ptr = 8;
 
-// 	printf("\n");
-// 	printf(" (%d)\n", printf("(0) P .00: |%.00i|", 0));
-// 	ft_printf(" (%d)\n", ft_printf("(0) P .00: |%.00i|", 0));
-// 	printf(" (%d)\n", printf("(0) P .01: |%.01i|", 0));
-// 	ft_printf(" (%d)\n", ft_printf("(0) P .01: |%.01i|", 0));
-// 	printf(" (%d)\n", printf("P .00: |%.00i|", di));
-// 	ft_printf(" (%d)\n", ft_printf("P .00: |%.00i|", di));
-// 	printf(" (%d)\n", printf("P .01: |%.01i|", di));
-// 	ft_printf(" (%d)\n", ft_printf("P .01: |%.01i|", di));
-// 	printf(" (%d)\n", printf("P .02: |%.02i|", di));
-// 	ft_printf(" (%d)\n", ft_printf("P .02: |%.02i|", di));
-// 	printf(" (%d)\n", printf("P .03: |%.03i|", di));
-// 	ft_printf(" (%d)\n", ft_printf("P .03: |%.03i|", di));
-// 	printf(" (%d)\n", printf("P .04: |%.04i|", di));
-// 	ft_printf(" (%d)\n", ft_printf("P .04: |%.04i|", di));
-// 	printf(" (%d)\n", printf("P .05: |%.05i|", di));
-// 	ft_printf(" (%d)\n", ft_printf("P .05: |%.05i|", di));
-// 	printf(" (%d)\n", printf("P .010: |%.010i|", di));
-// 	ft_printf(" (%d)\n", ft_printf("P .010: |%.010i|", di));
-// 	printf(" (%d)\n", printf("P .015: |%.015i|", di));
-// 	ft_printf(" (%d)\n", ft_printf("P .015: |%.015i|", di));
-// 	printf(" (%d)\n", printf("P .042: |%.042i|", di));
-// 	ft_printf(" (%d)\n", ft_printf("P .042: |%.042i|", di));
+// 	//my func
+// 	printf("\n%s--ft func--%s\n\n", RED, RESET);
+// 	ft_printf("\nreturn value = %d", ft_printf("Regular argument\n\n%c \n%.4s \n%p \n%.8d \n|%-9.06u| \n%.0d \n%X", 'X', "mahmut", &ptr, -1223, 999, 0, c));
+// 	ft_printf("\n");
 
-// 	printf(" (%d)\n", printf("P .007: |%.007i|", di));
-// 	ft_printf(" (%d)\n", ft_printf("P .007: |%.007i|", di));
-// 	printf(" (%d)\n", printf("P .000000000007: |%.000000000007i|", di));
-// 	ft_printf(" (%d)\n", ft_printf("P .000000000007: |%.000000000007i|", di));
+// 	// ft_printf("|%9.6u|\n", 999);
+// 	// ft_printf("|%c|\n", 'X');
+//printf(" X(%d)\n", printf("(0) 0: |%8c|", 'X'));
+//printf(" X(%d)\n", printf("(0) 0: |%08c|", 'X'));
 
-
-
-
+// 	//lib func
+// 	printf("\n%s--library func--%s\n\n", RED, RESET);
+// 	printf(  "\nreturn value = %d\n", printf("Regular argument\n\n%-c \n%.4s \n%p \n%.8d \n|%-9.06u| \n%.0d \n%X", 'X', "mahmut", &ptr, -1223, 999, 0, c));
 // 	return (0);
 // }
+
+int main(void)
+{
+	int				di;
+	di = 28;
+	int				ndi;
+	ndi = -28;
+
+	// width with integer
+	printf("\n%s/////////////////////////////////////////////////////////////////////////////////////////////////////%s\n\n", YELLOW, RESET);
+	printf("%sHandling minimum field width%s (controls the min number of characters to print)\n", PURPLE, RESET);
+	printf("%sA number follows flags or percentage sign > %%5d or %%23d\n\n%s", GREEN, RESET);
+	printf(" (%d)\n", printf("(0) Mfw 0: |%0i|", 0));
+	ft_printf(" (%d)\n", ft_printf("(0) Mfw 0: |%0i|", 0));
+	printf(" (%d)\n", printf("(0) Mfw 1: |%1i|", 0));
+	ft_printf(" (%d)\n", ft_printf("(0) Mfw 1: |%1i|", 0));
+	printf(" (%d)\n", printf("Mfw 0: |%0i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("Mfw 0: |%0i|", ndi));
+	printf(" (%d)\n", printf("Mfw 1: |%1i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("Mfw 1: |%1i|", ndi));
+	printf(" (%d)\n", printf("Mfw 2: |%2i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("Mfw 2: |%2i|", ndi));
+	printf(" (%d)\n", printf("Mfw 3: |%3i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("Mfw 3: |%3i|", ndi));
+	printf(" (%d)\n", printf("Mfw 4: |%4i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("Mfw 4: |%4i|", ndi));
+	printf(" (%d)\n", printf("Mfw 5: |%5i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("Mfw 5: |%5i|", ndi));
+	printf(" (%d)\n", printf("Mfw 10: |%10i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("Mfw 10: |%10i|", ndi));
+	printf(" (%d)\n", printf("Mfw 35: |%35i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("Mfw 35: |%35i|", ndi));
+
+
+	printf("\n%s/////////////////////////////////////////////////////////////////////////////////////////////////////%s\n\n", YELLOW, RESET);
+	printf("%sHandling precision%s (controls the max number of characters to print)\n", PURPLE, RESET);
+	printf("%sA dot (.) followed by a number > %%.5d or %%.02d\n\n%s", GREEN, RESET);
+
+	printf(" (%d)\n", printf("(0) P. 0: |%.0i|", 0));
+	ft_printf(" (%d)\n", ft_printf("(0) P. 0: |%.0i|", 0));
+	printf(" (%d)\n", printf("(0) P. 1: |%.1i|", 0));
+	ft_printf(" (%d)\n", ft_printf("(0) P. 1: |%.1i|", 0));
+	printf(" (%d)\n", printf("(0) P. 2: |%.2i|", 0));
+	ft_printf(" (%d)\n", ft_printf("(0) P. 2: |%.2i|", 0));
+	printf(" (%d)\n", printf("P. 0: |%.0i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("P. 0: |%.0i|", ndi));
+	printf(" (%d)\n", printf("P. 1: |%.1i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("P. 1: |%.1i|", ndi));
+	printf(" (%d)\n", printf("P. 2: |%.2i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("P. 2: |%.2i|", ndi));
+	printf(" (%d)\n", printf("P. 3: |%.3i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("P. 3: |%.3i|", ndi));
+	printf(" (%d)\n", printf("P. 4: |%.4i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("P. 4: |%.4i|", ndi));
+	printf(" (%d)\n", printf("P. 5: |%.5i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("P. 5: |%.5i|", ndi));
+	printf(" (%d)\n", printf("P. 10: |%.10i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("P. 10: |%.10i|", ndi));
+	printf(" (%d)\n", printf("P. 15: |%.15i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("P. 15: |%.15i|", ndi));
+	printf(" (%d)\n", printf("P. 42: |%.42i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("P. 42: |%.42i|", ndi));
+
+	printf("\n");
+	printf(" (%d)\n", printf("(0) P .00: |%.00i|", 0));
+	ft_printf(" (%d)\n", ft_printf("(0) P .00: |%.00i|", 0));
+	printf(" (%d)\n", printf("(0) P .01: |%.01i|", 0));
+	ft_printf(" (%d)\n", ft_printf("(0) P .01: |%.01i|", 0));
+	printf(" (%d)\n", printf("P .00: |%.00i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("P .00: |%.00i|", ndi));
+	printf(" (%d)\n", printf("P .01: |%.01i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("P .01: |%.01i|", ndi));
+	printf(" (%d)\n", printf("P .02: |%.02i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("P .02: |%.02i|", ndi));
+	printf(" (%d)\n", printf("P .03: |%.03i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("P .03: |%.03i|", ndi));
+	printf(" (%d)\n", printf("P .04: |%.04i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("P .04: |%.04i|", ndi));
+	printf(" (%d)\n", printf("P .05: |%.05i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("P .05: |%.05i|", ndi));
+	printf(" (%d)\n", printf("P .010: |%.010i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("P .010: |%.010i|", ndi));
+	printf(" (%d)\n", printf("P .015: |%.015i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("P .015: |%.015i|", ndi));
+	printf(" (%d)\n", printf("P .042: |%.042i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("P .042: |%.042i|", ndi));
+
+	printf(" (%d)\n", printf("P .007: |%.007i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("P .007: |%.007i|", ndi));
+	printf(" (%d)\n", printf("P .000000000007: |%.000000000007i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("P .000000000007: |%.000000000007i|", ndi));
+
+	printf("\n%s/////////////////////////////////////////////////////////////////////////////////////////////////////%s\n\n", YELLOW, RESET);
+	printf("%sHandling precision & minimum field width%s\n", PURPLE, RESET);
+	printf("%sNumbers followwed by a dot (.) followed by a number > %%10.5d or %%3.2d\n\n%s", GREEN, RESET);
+
+	printf(" (%d)\n", printf("(0) 0.0: |%0.0i|", 0));
+	ft_printf(" (%d)\n", ft_printf("(0) 0.0: |%0.0i|", 0));
+	printf(" (%d)\n", printf("(0) 1.0: |%1.0i|", 0));
+	ft_printf(" (%d)\n", ft_printf("(0) 1.0: |%1.0i|", 0));
+	printf(" (%d)\n", printf("(10) 1.0: |%1.0i|", 10));
+	ft_printf(" (%d)\n", ft_printf("(10) 1.0: |%1.0i|", 10));
+	printf(" (%d)\n", printf("(0) 0.1: |%0.1i|", 0));
+	ft_printf(" (%d)\n", ft_printf("(0) 0.1: |%0.1i|", 0));
+	printf(" (%d)\n", printf("(0) 1.1: |%1.1i|", 0));
+	ft_printf(" (%d)\n", ft_printf("(0) 1.1: |%1.1i|", 0));
+	printf(" (%d)\n", printf("0.0: |%0.0i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("0.0: |%0.0i|", ndi));
+	printf(" (%d)\n", printf("1.1: |%1.1i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("1.1: |%1.1i|", ndi));
+	printf(" (%d)\n", printf("2.2: |%2.2i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("2.2: |%2.2i|", ndi));
+	printf(" (%d)\n", printf("3.2: |%3.2i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("3.2: |%3.2i|", ndi));
+	printf(" (%d)\n", printf("2.3: |%2.3i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("2.3: |%2.3i|", ndi));
+	printf(" (%d)\n", printf("8.4: |%8.4i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("8.4: |%8.4i|", ndi));
+	printf(" (%d)\n", printf("4.8: |%4.8i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("4.8: |%4.8i|", ndi));
+	printf(" (%d)\n", printf("8.8: |%8.8i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("8.8: |%8.8i|", ndi));
+	printf(" (%d)\n", printf("8.2: |%8.2i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("8.2: |%8.2i|", ndi));
+	printf(" (%d)\n", printf("2.8: |%2.8i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("2.8: |%2.8i|", ndi));
+	printf(" (%d)\n", printf("8.1: |%8.1i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("8.1: |%8.1i|", ndi));
+	printf(" (%d)\n", printf("1.8: |%1.8i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("1.8: |%1.8i|", ndi));
+
+printf("\n%s/////////////////////////////////////////////////////////////////////////////////////////////////////%s\n\n", YELLOW, RESET);
+	printf("%sHandling precision & minimum field width & (0) flag%s\n", PURPLE, RESET);
+	printf("0 > Left-pads the number with zeroes (0) instead of spaces when padding is specified\n");
+	printf("%s0 followed by numbers & a dot+a number > %%010.5d or %%03.2d\n\n%s", GREEN, RESET);
+
+	printf(" (%d)\n", printf("(0) 00.0: |%00.0i|", 0));
+	ft_printf(" (%d)\n", ft_printf("(0) 00.0: |%00.0i|", 0));
+	printf(" (%d)\n", printf("(0) 01.0: |%01.0i|", 0));
+	ft_printf(" (%d)\n", ft_printf("(0) 01.0: |%01.0i|", 0));
+	printf(" (%d)\n", printf("(0) 00.1: |%00.1i|", 0));
+	ft_printf(" (%d)\n", ft_printf("(0) 00.1: |%00.1i|", 0));
+	printf(" (%d)\n", printf("01.1: |%01.1i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("01.1: |%01.1i|", ndi));
+	printf(" (%d)\n", printf("02.2: |%02.2i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("02.2: |%02.2i|", ndi));
+	printf(" (%d)\n", printf("03.2: |%03.2i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("03.2: |%03.2i|", ndi));
+	printf(" (%d)\n", printf("02.3: |%02.3i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("02.3: |%02.3i|", ndi));
+	printf(" (%d)\n", printf("08.4: |%08.4i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("08.4: |%08.4i|", ndi));
+	printf(" (%d)\n", printf("04.8: |%04.8i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("04.8: |%04.8i|", ndi));
+	printf(" (%d)\n", printf("08.8: |%08.8i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("08.8: |%08.8i|", ndi));
+	printf(" (%d)\n", printf("08.2: |%08.2i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("08.2: |%08.2i|", ndi));
+	printf(" (%d)\n", printf("02.8: |%02.8i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("02.8: |%02.8i|", ndi));
+	printf(" (%d)\n", printf("08.1: |%08.1i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("08.1: |%08.1i|", ndi));
+	printf(" (%d)\n", printf("01.8: |%01.8i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("01.8: |%01.8i|", ndi));
+
+
+	printf("\n%s/////////////////////////////////////////////////////////////////////////////////////////////////////%s\n\n", YELLOW, RESET);
+	printf("%sHandling minimum field width & (0) flag%s\n", PURPLE, RESET);
+	printf("0 > Left-pads the number with zeroes (0) instead of spaces when padding is specified\n");
+	printf("%s0 followed by numbers > %%010d or %%03d\n\n%s", GREEN, RESET);
+
+	printf(" (%d)\n", printf("(0) 0: |%0i|", 0));
+	ft_printf(" (%d)\n", ft_printf("(0) 0: |%0i|", 0));
+	printf(" (%d)\n", printf("(0) 00: |%00i|", 0));
+	ft_printf(" (%d)\n", ft_printf("(0) 00: |%00i|", 0));
+	printf(" (%d)\n", printf("(0) 01: |%01i|", 0));
+	ft_printf(" (%d)\n", ft_printf("(0) 01: |%01i|", 0));
+	printf(" (%d)\n", printf("(0) 02: |%02i|", 0));
+	ft_printf(" (%d)\n", ft_printf("(0) 02: |%02i|", 0));
+	printf(" (%d)\n", printf("0: |%0i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("0: |%0i|", ndi));
+	printf(" (%d)\n", printf("00: |%00i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("00: |%00i|", ndi));
+	printf(" (%d)\n", printf("01: |%01i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("01: |%01i|", ndi));
+	printf(" (%d)\n", printf("02: |%02i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("02: |%02i|", ndi));
+	printf(" (%d)\n", printf("03: |%03i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("03: |%03i|", ndi));
+	printf(" (%d)\n", printf("08: |%08i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("08: |%08i|", ndi));
+	printf(" (%d)\n", printf("000000000008: |%000000000008i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("000000000008: |%000000000008i|", ndi));
+	printf(" (%d)\n", printf("010: |%010i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("010: |%010i|", ndi));
+	printf(" (%d)\n", printf("0000000000010: |%0000000000010i|", ndi));
+	ft_printf(" (%d)\n", ft_printf("0000000000010: |%0000000000010i|", ndi));
+	return (0);
+}
